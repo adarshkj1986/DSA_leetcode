b861c3c62f4a5c886c0b69a85d5f7fd2996fee09
Adarsh Kumar Jha <adarshkj1986@gmail.com>
2026-08-16 17:30:15 +0530

    Time: 73 ms (9.41%) | Memory: 14.2 MB (73.31%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,8 +6,8 @@ public:
         for(int i=0;i<nums.size()-1;i++){
            for(int j=i+1;j<nums.size();j++){
              if(nums[i]+nums[j]==target){
-                result.push_back(i);
-                result.push_back(j);
+                result.emplace_back(i);
+                result.emplace_back(j);
                 return result;
               
             }
